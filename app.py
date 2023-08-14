5306110a5e90f2eeafc2b942d5c6936be28a63db
Manasa Kalaimalai <manasastyles@gmail.com>
2023-08-14 22:05:14 +0530

    fixed tags & added chatbot

--- app.py
+++ app.py
@@ -14,20 +14,26 @@ def submit():
     option3 = request.form['option3']
     option4 = request.form['option4']
     option5 = request.form['option5']
-    # Retrieve other options similarly
+    
+    print("Option 1:", option1)
+    print("Option 2:", option2)
+    print("Option 3:", option3)
+    print("Option 4:", option4)
+    print("Option 5:", option5)
 
-    # Process the selected options as needed
+    # Retrieve other options similarly
 
     db = sqlite3.connect('search_history.db')
     cursor = db.cursor()
     cursor.execute("SELECT name FROM sqlite_master WHERE type = 'table' AND name=?", ('history',))
     result = cursor.fetchone()
-    if(result is None):
+    if result is None:
         cursor.execute('''CREATE TABLE history(gender varchar(20), garment varchar(20), brand varchar(20), fabric varchar(20), color varchar(20))''')
     cursor.execute("INSERT into history(gender, garment, brand, fabric, color) VALUES (?, ?, ?, ?, ?)", (option1, option2, option3, option4, option5))
     db.commit()
     db.close()
     return render_template('home.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,31 +1,17 @@
 function toggleDropdown() {
     var dropdown = document.getElementById("dropdown");
-    dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
+    dropdown.style.display = (dropdown.style.display === "block") ? "none" : "block";
 }
 
-
-// JavaScript to handle tag creation and search
-document.addEventListener("DOMContentLoaded", function () {
-    const selects = document.querySelectorAll(".dropdown select");
-    const searchInput = document.getElementById("search");
+function populateSearchBar(optionId) {
+    var selectedOption = document.getElementById(optionId);
+    var searchInput = document.getElementById('search');
+    var currentSearchValue = searchInput.value;
     
-    selects.forEach(select => {
-        select.addEventListener("change", function () {
-            const selectedOption = select.options[select.selectedIndex];
-            
-            if (selectedOption.value !== "") {
-                const tagText = selectedOption.text;
-                const currentValue = searchInput.value.trim();
-                
-                if (currentValue === "") {
-                    searchInput.value = tagText;
-                } else {
-                    searchInput.value = currentValue + ", " + tagText;
-                }
-                
-                selectedOption.disabled = true;
-                select.selectedIndex = 0;
-            }
-        });
-    });
-});
+    // Append the selected option value to the search bar
+    if (currentSearchValue === '') {
+        searchInput.value = selectedOption.value;
+    } else {
+        searchInput.value = currentSearchValue + ', ' + selectedOption.value;
+    }
+}
