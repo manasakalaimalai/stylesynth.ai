9f198596d48bb3f131b76eb6b35961328037b499
Abraham <abanel@Abrahams-MacBook-Air.local>
2023-08-19 09:53:06 +0530

    fixed some bugs

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def generate_response(prompt):
     response = model.generate(prompt, max_tokens=150)  # Replace with actual model generation code
     return response
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/chat', methods=['GET', 'POST'])
 def chat():
     response = []
     generated_response = None
@@ -63,48 +63,54 @@ def chat():
 
     return render_template('outfit.html', response=response, generated_response=generated_response, user_input=user_input, stable_output=stable_output, search_results = image_results)
 
-@app.route('/submit', methods=['POST'])
+@app.route('/hola', methods=['GET', 'POST'])
+def hola():
+    return render_template('home.html')
+
+@app.route('/', methods=['GET', 'POST'])
 def submit():
-    option1 = request.form["option1"]
-    option2 = request.form["option2"]
-    option3 = request.form["option3"]
-    option4 = request.form["option4"]
-    option5 = request.form["option5"]
+    image_results = []
+    if request.method == 'POST':
+        option1 = request.form["option1"]
+        option2 = request.form["option2"]
+        option3 = request.form["option3"]
+        option4 = request.form["option4"]
+        option5 = request.form["option5"]
 
-    options = [option1, option2, option3, option4, option5]
+        options = [option1, option2, option3, option4, option5]
 
-    query = " ".join(opt for opt in options if opt != "Any")
+        query = " ".join(opt for opt in options if opt != "Any")
 
-    print(query)
+        print(query)
 
-    params = {
-    "api_key": "1260231f7bba0c7bfc657de963706e2d1deaade93938c3cd588f22ab7d195ab8",
-    "engine": "google_images",
-    "safe": "active",
-    "q": query,  # Use the filtered query here
-    "google_domain": "google.co.in",
-    "gl": "in",
-    "hl": "en",
-    "location": "Bengaluru, Karnataka, India"
-    }
+        params = {
+        "api_key": "1260231f7bba0c7bfc657de963706e2d1deaade93938c3cd588f22ab7d195ab8",
+        "engine": "google_images",
+        "safe": "active",
+        "q": query,  # Use the filtered query here
+        "google_domain": "google.co.in",
+        "gl": "in",
+        "hl": "en",
+        "location": "Bengaluru, Karnataka, India"
+        }
 
-    search = GoogleSearch(params)
-    results = search.get_dict()
+        search = GoogleSearch(params)
+        results = search.get_dict()
 
-    image_results = results["images_results"][:10]
+        image_results = results["images_results"][:10]
 
 
-    # Process the selected options as needed
+        # Process the selected options as needed
 
-    db = sqlite3.connect('search_history.db')
-    cursor = db.cursor()
-    cursor.execute("SELECT name FROM sqlite_master WHERE type = 'table' AND name=?", ('history',))
-    result = cursor.fetchone()
-    if(result is None):
-        cursor.execute('''CREATE TABLE history(gender varchar(20), garment varchar(20), brand varchar(20), fabric varchar(20), color varchar(20))''')
-    cursor.execute("INSERT into history(gender, garment, brand, fabric, color) VALUES (?, ?, ?, ?, ?)", (option1, option2, option3, option4, option5))
-    db.commit()
-    db.close()
+        db = sqlite3.connect('search_history.db')
+        cursor = db.cursor()
+        cursor.execute("SELECT name FROM sqlite_master WHERE type = 'table' AND name=?", ('history',))
+        result = cursor.fetchone()
+        if(result is None):
+            cursor.execute('''CREATE TABLE history(gender varchar(20), garment varchar(20), brand varchar(20), fabric varchar(20), color varchar(20))''')
+        cursor.execute("INSERT into history(gender, garment, brand, fabric, color) VALUES (?, ?, ?, ?, ?)", (option1, option2, option3, option4, option5))
+        db.commit()
+        db.close()
     return render_template('home.html', search_results=image_results)
 
 
